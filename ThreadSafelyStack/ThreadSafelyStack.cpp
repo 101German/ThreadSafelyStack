171f5a8e099cf828d6ac93f15f2c4e26bbfad9fb
gervika@list.ru <gervika@list.ru>
2021-07-11 15:42:03 +0300

    first commit

---
+++ ThreadSafelyStack/ThreadSafelyStack.cpp
@@ -0,0 +1,68 @@
+﻿
+
+#include <iostream>
+#include <stack>
+#include <thread>
+#include<mutex>
+#include <exception>
+
+
+
+
+struct empty_stack:std::exception
+{
+	const char* what() const throw();
+
+};
+
+template<typename T>
+class ThreadSafelyStack
+{
+public:
+	
+	ThreadSafelyStack(){}
+	
+
+	ThreadSafelyStack(const ThreadSafelyStack& other)
+	{
+		std::lock_guard<std::mutex> lock(other.mtx);
+		stack = other.stack;
+	}
+	void Push(T value)
+	{
+		std::lock_guard<std::mutex> lock(mtx);
+		stack.push(std::move(value));
+	}
+
+	std::shared_ptr<T> pop() 
+	{
+		std::lock_guard<std::mutex> lock(mtx);
+		if (stack.empty())
+			throw empty_stack();
+		std::shared_ptr<T> const res(std::make_shared<T>(std::move(stack.top())));
+		stack.pop();
+		return res;
+	}
+
+	bool empty()const 
+	{
+		std::lock_guard<std::mutex> lock(mtx);
+		return stack.empty();
+	}
+
+private:
+	std::stack<T> stack;
+	mutable std::mutex mtx;
+};
+
+
+
+int main()
+{
+	
+}
+
+const char* empty_stack::what() const throw()
+{
+	return "stack is empty";
+}
